b7b0cdc065dca4276de030ff58869165a063fd7f
kryo <mail@fabiosousa.com>
2012-09-01 22:57:05 +0100

    fixed a bug when capturing achievements

--- Cyui/modules/misc/screenie.lua
+++ Cyui/modules/misc/screenie.lua
@@ -3,10 +3,10 @@ local T, C, L, G = unpack(Tukui)
 if not C.utilities.screenie then return end
 
 -- delay function from http://www.wowwiki.com/Wait
-local waitTable = {}
-local function AchievementScreenshot(delay, func, ...)
-	waitFrame = CreateFrame("Frame","WaitFrame", UIParent)
-	waitFrame:SetScript("onUpdate",function (self, elapse)
+local function autoscreenshot(delay, func, ...)
+	local waitTable = {}
+	local waitFrame = CreateFrame("Frame", "WaitFrame", UIParent)
+	waitFrame:SetScript("onUpdate", function (self, elapse)
 		local count = #waitTable
 		local i = 1
 		while (i <= count) do
@@ -15,7 +15,7 @@ local function AchievementScreenshot(delay, func, ...)
 			local f = tremove(waitRecord, 1)
 			local p = tremove(waitRecord, 1)
 			if (d > elapse) then
-				tinsert(waitTable, i, {d-elapse, f, p})
+				tinsert(waitTable, i, {d - elapse, f, p})
 				i = i + 1
 			else
 				count = count - 1
@@ -27,9 +27,35 @@ local function AchievementScreenshot(delay, func, ...)
 end
 
 local function OnEvent(...)
-	AchievementScreenshot(2, Screenshot)
+	autoscreenshot(1, Screenshot)
 end
 
-local yScreenie = CreateFrame("Frame")
-yScreenie:RegisterEvent("ACHIEVEMENT_EARNED")
-yScreenie:SetScript("OnEvent", OnEvent)
+local Screenie = CreateFrame("Frame")
+Screenie:RegisterEvent("ACHIEVEMENT_EARNED")
+Screenie:SetScript("OnEvent", OnEvent)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
